ad5109ea71984cd00694972582b486d5abc397db
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-31 18:44:00 +0700

    ui

--- src/app/components/friend.component.ts
+++ src/app/components/friend.component.ts
@@ -1,19 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Request } from '../services/request.service';
+import { UserInfo } from '../types';
 
 @Component({
     template: `
         <h4>Friend Component</h4>
-    `
+        <div *ngFor="let user of users">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" [src]="user.avatar" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">{{ user.name }}</h5>
+                  <a href="#" class="btn btn-primary">Go to profile</a>
+                </div>
+            </div>
+        </div>
+    `,
+    styles: [`
+        .card {
+            padding: 10px;
+            border: solid 1px #D1D1D1;
+            border-radius: 5px;
+            margin: 5px;
+        }
+    `]
 })
 
 export class FriendComponent implements OnInit {
+    users: UserInfo[] = [];
     constructor(private request: Request) {}
 
     ngOnInit() {
         this.request.get('/user')
-        .then(response => {
-            console.log(response.users);
-        });
+        .then(response => this.users = response.users);
     }
 }
