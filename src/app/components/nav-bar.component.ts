1d58a3ced41ae60525ef676335a8f6169d3722ab
Van Pho Nguyen <vanpho02@gmail.com>
2018-06-02 18:57:46 +0700

    isSignedIn

--- src/app/components/nav-bar.component.ts
+++ src/app/components/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
     selector: 'app-nav-bar',
@@ -8,12 +9,16 @@ import { Component } from '@angular/core';
               <div class="navbar-header">
                 <a class="navbar-brand" href="#">Khoa Pham Page</a>
               </div>
-              <ul class="nav navbar-nav">
+              <ul class="nav navbar-nav" *ngIf="!isSignedIn">
                 <li><a routerLink="/" routerLinkActive="active" [routerLinkActiveOptions]="{exact: true}">Home</a></li>
                 <li><a routerLink="/signin" routerLinkActive="active">Sign In</a></li>
                 <li><a routerLink="/signup" routerLinkActive="active">Sign Up</a></li>
+              </ul>
+              <ul class="nav navbar-nav" *ngIf="isSignedIn">
+                <li><a routerLink="/" routerLinkActive="active" [routerLinkActiveOptions]="{exact: true}">Home</a></li>
                 <li><a routerLink="/profile" routerLinkActive="active">Profile</a></li>
                 <li><a routerLink="/friend" routerLinkActive="active">Friend</a></li>
+                <li><a routerLink="#" (click)="userService.logOut();">Log Out</a></li>
               </ul>
             </div>
         </nav>
@@ -26,4 +31,8 @@ import { Component } from '@angular/core';
     `]
 })
 
-export class NavBarComponent {}
+export class NavBarComponent {
+    constructor(public userService: UserService) {}
+
+    get isSignedIn() { return !!localStorage.token; }
+}
